33b2cfe5230bea04da4c92fada7d31787c21fa5a
na-jimenezc <141644016+na-jimenezc@users.noreply.github.com>
2025-02-08 20:43:57 -0500

    CAMBIO FINAL

--- codigoCliente.java
+++ codigoCliente.java
@@ -1,28 +1,19 @@
-import java.io.*;
+    import java.io.*;
 	import java.net.*;
 	import java.util.Scanner;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> dcac7d5 (Subiendo todos los cambios realizados)
 	public class codigoCliente {
 		public static void main(String[] args) throws IOException {
-			
 			
 			Scanner scanner = new Scanner(System.in);
 			System.out.println("Sumador de digitos de un vector ");
 			System.out.println("\nIngrese el vector separando los números por comas (,):");
 			String input = scanner.nextLine();
-			
-<<<<<<< HEAD
-			String datos = input.replace(",", "");
-=======
-			String datos = input.replace(",","");
->>>>>>> dcac7d5 (Subiendo todos los cambios realizados)
-			
+		
+			String datos = input;
+
 			/*Para conectar con el servidor de cálculo*/
-			Socket socket = new Socket("10.0.10.159", 5000); 
+			Socket socket = new Socket("25.64.244.102", 5000); 
 			
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
