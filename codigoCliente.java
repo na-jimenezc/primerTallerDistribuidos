2bbf91352f2235dde658ad7d73693b7419cc4d30
na-jimenezc <141644016+na-jimenezc@users.noreply.github.com>
2025-02-15 13:14:12 +0000

    MANEJO DE ERRORES EN CASO DE NO CONEXIÓN CON EL CENTRO DE CÁLCULO

--- codigoCliente.java
+++ codigoCliente.java
@@ -12,18 +12,41 @@
 		
 			String datos = input;
 
-			/*Para conectar con el servidor de cálculo*/
-			Socket socket = new Socket("25.64.244.102", 5000); 
-			
-			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-			out.println(datos);
+			while(true){
+				try {
+					/*Intentar conectar con el servidor de cálculo*/
 
-			/*Para recibir el resulado final*/
-			String respuesta = in.readLine();
-			System.out.println("Suma total: " + respuesta);
+					/*Se pone la ip y el socket*/
+					Socket socket = new Socket("25.64.244.102", 5000);
+					PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+					BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	
+					/*Enviar datos*/
+					out.println(datos);
+	
+					/*Se recibe el resultado final*/
+					String respuesta = in.readLine();
+					System.out.println("Suma total: "+respuesta);
+	
+					/*Se cierra la conexión cerrando el socket y se sale del while*/
+					socket.close();
+					break;
 
-			socket.close();
+				}catch(IOException e){
+					System.out.println("El centro de cálculo no se encuentra disponible en este momento.");
+					System.out.println("¿Quiere seguir intentando? (s/n)");
+
+					String opcion = scanner.nextLine().trim().toLowerCase();
+					
+					if (!opcion.equals("s")) {
+						System.out.println("Intento finalizado, no fue posible establecer la conexión con el centro de cálculo");
+						break;
+					}
+				}
+			}
+
+			scanner.close();
+
 		}
 	}
